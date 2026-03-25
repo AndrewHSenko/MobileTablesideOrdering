e7dce6956b796c28618cef317a5196771f3535f7
Andrew Senkowski <51846520+AndrewHSenko@users.noreply.github.com>
2026-03-25 17:25:52 -0400

    Bringing footer text closer to bottom

--- frontend/src/components/Icon.jsx
+++ frontend/src/components/Icon.jsx
@@ -9,7 +9,7 @@ const Icon = ({dest, image, text}) => {
     <>
       <Link to={dest} onClick={() => {text === "Language" ? setShowToast(true) : null}} className="d-flex flex-column align-items-center text-center link-underline-altblue text-black">
         <img src={image} alt={`${text} icon`} className="svg-icon" style={{"--icon-size" : "36px"}}/>
-        <p className="mb-1 fs-6">{text}</p>
+        <p className="fs-6">{text}</p>
       </Link>
       <ToastContainer position="bottom-end" className="p-3" style={{ zIndex: 9999 }}>
           <Toast
