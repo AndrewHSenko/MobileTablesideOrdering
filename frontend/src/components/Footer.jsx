989088bd52149ecf1c867adeadf4afc21f6dced9
Andrew Senkowski <51846520+AndrewHSenko@users.noreply.github.com>
2026-03-25 17:23:21 -0400

    Minor adjustments to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,10 +7,10 @@ import Icon from '../components/Icon'
 
 const Footer = () => {
   return (
-    <div className="d-flex justify-content-between align-items-center lead fs-6 px-3 pt-2 pb-1 z-1 bg-altblue sticky-bottom">
+    <div className="d-flex justify-content-between align-items-center lead fs-6 px-3 pt-2 z-1 bg-altblue sticky-bottom">
       <Icon dest="/" image={home} text="Home"/>
       <Icon dest="checkout" image={shopping_cart} text="Checkout"/>
-      <Icon dest="" image={world} text="Language"/>
+      <Icon dest={null} image={world} text="Language"/>
       <a href="https://www.hmshost.com/">
         <img src={HMS} alt="HMSHost Logo" className="logo-img"/>
       </a>
