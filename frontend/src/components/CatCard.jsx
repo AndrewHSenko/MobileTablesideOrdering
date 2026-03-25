3663c68dcd3c6665df33fcea0a6bd99be29bfed2
Andrew Senkowski <51846520+AndrewHSenko@users.noreply.github.com>
2026-03-25 18:24:06 -0400

    Update CatCard.jsx

--- frontend/src/components/CatCard.jsx
+++ frontend/src/components/CatCard.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router'
 
 const CatCard = ({image, title}) => {
   return (
-    <Link to="/menu" state={{activeMenu : title.toLowerCase()}} className="card text-center text-decoration-none rounded-3 my-2">
+    <Link to="/menu" state={{active : title.toLowerCase()}} className="card text-center text-decoration-none rounded-3 my-2">
         <img src={image} alt={`${title} picture`} className="card-img-top"/>
         <h4 className="card-title lead my-1">{title}</h4>
     </Link>
