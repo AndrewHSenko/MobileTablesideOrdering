0590b28e250a2f697e6f8e90e81470f13ef27dd5
Andrew Senkowski <51846520+AndrewHSenko@users.noreply.github.com>
2026-03-29 14:41:11 -0400

    Footer top border

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ import Icon from '../components/Icon'
 
 const Footer = () => {
   return (
-    <div className="d-flex justify-content-between align-items-center lead fs-6 px-3 pt-1 z-1 bg-lightbg sticky-bottom">
+    <div className="d-flex justify-content-between align-items-center lead fs-6 px-3 pt-1 z-1 bg-lightbg sticky-bottom border-top border-1 border-dark-subtle">
       <Icon dest="/" image={home} text="Home"/>
       <Icon dest="checkout" image={shopping_cart} text="Checkout"/>
       <Icon dest={null} image={world} text="Language"/>
